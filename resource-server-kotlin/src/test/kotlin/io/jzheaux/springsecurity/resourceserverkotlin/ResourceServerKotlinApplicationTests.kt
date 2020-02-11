d08bf4c97bb934c8c8f5d20d0630246b2a999ad1
Josh Cummings <josh.cummings@gmail.com>
2020-02-11 11:03:26 -0700

    Polished test

--- resource-server-kotlin/src/test/kotlin/io/jzheaux/springsecurity/resourceserverkotlin/ResourceServerKotlinApplicationTests.kt
+++ resource-server-kotlin/src/test/kotlin/io/jzheaux/springsecurity/resourceserverkotlin/ResourceServerKotlinApplicationTests.kt
@@ -3,20 +3,20 @@ package io.jzheaux.springsecurity.resourceserverkotlin
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient
-import org.springframework.boot.test.context.SpringBootTest
-import org.springframework.test.web.reactive.server.WebTestClient
+import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest
 import org.springframework.security.test.web.reactive.server.SecurityMockServerConfigurers.mockJwt
+import org.springframework.test.web.reactive.server.WebTestClient
 
-@SpringBootTest
+@WebFluxTest
 @AutoConfigureWebTestClient
 class ResourceServerKotlinApplicationTests(@Autowired val web: WebTestClient) {
 
 	@Test
 	fun getWhenJwtThenOk() {
-		this.web.mutateWith(mockJwt().jwt { jwt -> jwt.subject("sub") })
+		this.web.mutateWith(mockJwt().jwt { j -> j.subject("subject") })
 				.get().uri("/")
 				.exchange()
-				.expectStatus().isOk;
+				.expectStatus().isOk
 	}
 
 }
